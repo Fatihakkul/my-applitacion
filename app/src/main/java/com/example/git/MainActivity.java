5fa359be7bc1e3dceafdc20012d13d36215c1b9b
Fatihakkul <63662935+Fatihakkul@users.noreply.github.com>
2020-04-14 23:47:32 +0300

    text eklendi

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 Button bn;
 int x;
+TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bn=findViewById(R.id.btn);
+        txt=findViewById(R.id.txt);
         x=5;
     }
 }
