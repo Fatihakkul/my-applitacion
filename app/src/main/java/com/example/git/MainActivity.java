9abef9d8fd548d7b8a390a59fa496fb0ca61a9cc
Fatihakkul <63662935+Fatihakkul@users.noreply.github.com>
2020-04-14 23:10:16 +0300

    Buton eklendi

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.git;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button bn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bn=findViewById(R.id.btn);
     }
 }
