c6099c7da6878a23e5ce8c60a160d5d5ffae1e86
Fatihakkul <63662935+Fatihakkul@users.noreply.github.com>
2020-04-15 00:02:51 +0300

    bra3

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -7,15 +7,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-Button bn;
-int x;
-TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bn=findViewById(R.id.btn);
-        txt=findViewById(R.id.txt);
-        x=5;
+
     }
 }
